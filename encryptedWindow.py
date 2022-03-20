3c310a53c2063520c7bf42051766b0f002e29dcd
User01 <romansochirca30@gmail.com>
2022-03-20 14:57:22 +0200

    Ver0.2 add mainWindow, add encryptedWindow

---
+++ encryptedWindow.py
@@ -0,0 +1,205 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QMessageBox
+import numpy as np
+import PIL.Image
+
+
+class Ui_encryptedWindow(object):
+    def setupUi(self, encryptedWindow):
+        encryptedWindow.setObjectName("encryptedWindow")
+        encryptedWindow.resize(461, 376)
+        font = QtGui.QFont()
+        font.setItalic(False)
+        encryptedWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(encryptedWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.addImage = QtWidgets.QPushButton(self.centralwidget)
+        self.addImage.setGeometry(QtCore.QRect(250, 230, 171, 51))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.addImage.setFont(font)
+        self.addImage.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
+        self.addImage.setObjectName("addImage")
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(170, 180, 121, 21))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.comboBox.setFont(font)
+        self.comboBox.setObjectName("comboBox")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 0, 441, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 30, 461, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.addText = QtWidgets.QPushButton(self.centralwidget)
+        self.addText.setGeometry(QtCore.QRect(40, 230, 171, 51))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.addText.setFont(font)
+        self.addText.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
+        self.addText.setObjectName("addText")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 60, 451, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.label_3.setFont(font)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(10, 90, 331, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        self.inputPassword = QtWidgets.QLineEdit(self.centralwidget)
+        self.inputPassword.setGeometry(QtCore.QRect(40, 340, 131, 21))
+        self.inputPassword.setObjectName("inputPassword")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(10, 120, 331, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.label_5.setFont(font)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(40, 310, 111, 31))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(11)
+        self.label_6.setFont(font)
+        self.label_6.setObjectName("label_6")
+        self.confirmBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.confirmBtn.setGeometry(QtCore.QRect(310, 330, 111, 31))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.confirmBtn.setFont(font)
+        self.confirmBtn.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
+        self.confirmBtn.setObjectName("confirmBtn")
+        encryptedWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(encryptedWindow)
+        QtCore.QMetaObject.connectSlotsByName(encryptedWindow)
+
+        self.add_functions()
+
+    def retranslateUi(self, encryptedWindow):
+        _translate = QtCore.QCoreApplication.translate
+        encryptedWindow.setWindowTitle(_translate("encryptedWindow", "Ascunderea Textului"))
+        self.addImage.setText(_translate("encryptedWindow", "Alegeti o imagine"))
+        self.comboBox.setItemText(0, _translate("encryptedWindow", "1 culoarae"))
+        self.comboBox.setItemText(1, _translate("encryptedWindow", "2 culori"))
+        self.comboBox.setItemText(2, _translate("encryptedWindow", "3 culori"))
+        self.label.setText(_translate("encryptedWindow", "Pas 1: Alegeti numarul de culori pentru ascundere (1-3)"))
+        self.label_2.setText(_translate("encryptedWindow", "Pas 2: Alegeti un fisier text care va fi inserat in imagine (.txt)"))
+        self.addText.setText(_translate("encryptedWindow", "Alegeti un text"))
+        self.label_3.setText(_translate("encryptedWindow", "Pas 3: Alegeti imaginea in care va fi ascuns textul (.jpg.png)"))
+        self.label_4.setText(_translate("encryptedWindow", "Pas 4: Introduceti o parola pentru securitate"))
+        self.label_5.setText(_translate("encryptedWindow", "Pas 5: Confirmati alegerea"))
+        self.label_6.setText(_translate("encryptedWindow", "Parola"))
+        self.confirmBtn.setText(_translate("encryptedWindow", "Confirmati"))
+
+    def add_functions(self):
+        self.messageToHide = ""
+        self.imagePath = ""
+        self.addText.clicked.connect(lambda: self.openTextFile())
+        self.addImage.clicked.connect(lambda: self.openImagePath())
+        self.confirmBtn.clicked.connect(lambda: self.encryptedTextToImage())
+
+
+    def openTextFile(self):
+        file, _ = QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()", "", "Text Files (*.txt)")
+        with open(file) as f:
+            data = f.read()
+
+        if data == "":
+            error = QMessageBox()
+            error.setWindowTitle("Error")
+            error.setText("Fisierul text ales este gol!")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Ok)
+            error.exec_()
+        else:
+            self.messageToHide = data
+
+    def openImagePath(self):
+        file, _ = QFileDialog.getOpenFileName(None, "QFileDialog.getOpenFileName()", "", "jpg images (*.jpg);;png images (*.png)")
+        self.imagePath = file
+
+
+    def encryptedTextToImage(self):
+        if self.messageToHide == "":
+            error = QMessageBox()
+            error.setWindowTitle("Error")
+            error.setText("Fisierul text nu este incarcat!")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Ok)
+            error.exec_()
+        elif self.inputPassword.text() == "":
+            error = QMessageBox()
+            error.setWindowTitle("Error")
+            error.setText("Nu a fost introdusa parola!")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Ok)
+            error.exec_()
+        elif self.imagePath == "":
+            error = QMessageBox()
+            error.setWindowTitle("Error")
+            error.setText("Nu a fost incarcata imaginea!")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Ok)
+            error.exec_()
+        else:
+            image = PIL.Image.open(self.imagePath, "r")
+            width, height = image.size
+            img_arr = np.array(list(image.getdata()))
+
+            channels = 4 if image.mode == "RGBA" else 3
+            pixels = img_arr.size // channels
+
+            stop_indicator = f"${self.inputPassword.text()}$"
+            stop_indicator_length = len(stop_indicator)
+
+            self.messageToHide += stop_indicator
+            byte_message = "".join(f"{ord(c):08b}" for c in self.messageToHide)
+            bits = len(byte_message)
+
+            if bits > pixels:
+                error = QMessageBox()
+                error.setWindowTitle("Error")
+                error.setText("Imaginea este prea mica pentru a ascunde textul!")
+                error.setIcon(QMessageBox.Warning)
+                error.setStandardButtons(QMessageBox.Ok)
+                error.exec_()
+                return 0
+            else:
+                index = 0
+                for i in range(pixels):
+                    for j in range(0, 3):
+                        if index < bits:
+                            img_arr[i][j] = int(bin(img_arr[i][j])[2:-1] + byte_message[index], 2)
+                            index += 1
+                img_arr = img_arr.reshape((height, width, channels))
+                result = PIL.Image.fromarray(img_arr.astype('uint8'), image.mode)
+                result.save("encoded.png")
+
+                success = QMessageBox()
+                success.setWindowTitle("Succes")
+                success.setText("Imaginea a fost criptata cu succes")
+                success.setIcon(QMessageBox.Information)
+                success.setStandardButtons(QMessageBox.Ok)
+                success.exec_()
+
+
--- main.py
+++ main.py
@@ -1,37 +1,144 @@
-import numpy as np
-import PIL.Image
+from PyQt5 import QtCore, QtGui, QtWidgets
+from encryptedWindow import Ui_encryptedWindow
 
-message_to_hide = "My name is Roman and I am a student!"
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setEnabled(True)
+        MainWindow.resize(490, 339)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        MainWindow.setFont(font)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.text_1Lb = QtWidgets.QLabel(self.centralwidget)
+        self.text_1Lb.setGeometry(QtCore.QRect(10, 280, 221, 31))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(True)
+        font.setUnderline(False)
+        font.setWeight(50)
+        font.setStrikeOut(False)
+        font.setKerning(True)
+        self.text_1Lb.setFont(font)
+        self.text_1Lb.setObjectName("text_1Lb")
+        self.imageLb = QtWidgets.QLabel(self.centralwidget)
+        self.imageLb.setGeometry(QtCore.QRect(10, 10, 251, 251))
+        self.imageLb.setText("")
+        self.imageLb.setPixmap(QtGui.QPixmap("background/monalisa_bg.jpg"))
+        self.imageLb.setObjectName("imageLb")
+        self.text_2Lb = QtWidgets.QLabel(self.centralwidget)
+        self.text_2Lb.setGeometry(QtCore.QRect(10, 310, 391, 21))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(10)
+        font.setItalic(True)
+        self.text_2Lb.setFont(font)
+        self.text_2Lb.setObjectName("text_2Lb")
+        self.encryptedBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.encryptedBtn.setGeometry(QtCore.QRect(310, 40, 141, 51))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.encryptedBtn.setFont(font)
+        self.encryptedBtn.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
+        self.encryptedBtn.setStyleSheet("")
+        self.encryptedBtn.setObjectName("encryptedBtn")
+        self.decryptedBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.decryptedBtn.setGeometry(QtCore.QRect(310, 110, 141, 51))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.decryptedBtn.setFont(font)
+        self.decryptedBtn.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
+        self.decryptedBtn.setStyleSheet("")
+        self.decryptedBtn.setObjectName("decryptedBtn")
+        self.exitBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.exitBtn.setGeometry(QtCore.QRect(310, 180, 141, 51))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setItalic(False)
+        font.setUnderline(False)
+        font.setWeight(75)
+        self.exitBtn.setFont(font)
+        self.exitBtn.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
+        self.exitBtn.setStyleSheet("")
+        self.exitBtn.setObjectName("exitBtn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.actionopen = QtWidgets.QAction(MainWindow)
+        self.actionopen.setObjectName("actionopen")
+        self.actionnew = QtWidgets.QAction(MainWindow)
+        self.actionnew.setObjectName("actionnew")
+        self.actionSave_as = QtWidgets.QAction(MainWindow)
+        self.actionSave_as.setObjectName("actionSave_as")
 
-image = PIL.Image.open("img/mona-lisa.jpg", "r")
-width, height = image.size
-img_arr = np.array(list(image.getdata()))
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-if image.mode == "P":
-    print("Not supported")
-    exit()
+        self.add_functions()
 
-channels = 4 if image.mode == "RGBA" else 3
-pixels = img_arr.size // channels
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Steganografie"))
+        self.text_1Lb.setText(_translate("MainWindow", "Creat de:"))
+        self.text_2Lb.setText(_translate("MainWindow", "Roman Sochirca IS21Z USARB"))
+        self.encryptedBtn.setText(_translate("MainWindow", "Ascunde"))
+        self.decryptedBtn.setText(_translate("MainWindow", "Descifreza"))
+        self.exitBtn.setText(_translate("MainWindow", "Iesire"))
+        self.actionopen.setText(_translate("MainWindow", "open"))
+        self.actionnew.setText(_translate("MainWindow", "new"))
+        self.actionSave_as.setText(_translate("MainWindow", "Save as"))
 
-stop_indicator = "$STOP$"
-stop_indicator_length = len(stop_indicator)
 
-message_to_hide += stop_indicator
+    def add_functions(self):
+        self.encryptedBtn.clicked.connect(lambda: self.openEncryptedWindow())
+        self.decryptedBtn.clicked.connect(lambda: self.openDecryptedWindow())
+        self.exitBtn.clicked.connect(lambda: self.exitFromApp())
 
-byte_message = "".join(f"{ord(c):08b}" for c in message_to_hide)
-bits = len(byte_message)
 
-if bits > pixels:
-    print("Not enough space")
-else:
-    index = 0
-    for i in range(pixels):
-        for j in range(0, 3):
-            if index < bits:
-                img_arr[i][j] = int(bin(img_arr[i][j])[2:-1] + byte_message[index], 2)
-                index += 1
 
-img_arr = img_arr.reshape((height, width, channels))
-result = PIL.Image.fromarray(img_arr.astype('uint8'), image.mode)
-result.save("encoded.png")
+    def openEncryptedWindow(self):
+        self.ui2 = mywindow2()
+        self.ui2.show()
+
+
+    def openDecryptedWindow(self):
+        print("decrypt")
+
+    def exitFromApp(self):
+        print("exit")
+
+
+class mywindow2(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow2, self).__init__()
+        self.ui2 = Ui_encryptedWindow()
+        self.ui2.setupUi(self)
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
